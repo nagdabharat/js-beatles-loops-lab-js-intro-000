714f03892073d6dd52d2d0c42d7bcd2255f89438
Bharat Nagda <nagdabharat@gmail.com>
2020-06-13 13:48:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var temparray = [];
+  for (let i = 0; i < musicians.length; i++) {
+    temparray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return temparray;
+}
+
+function johnLennonFacts(array) {
+  var temporary = [];
+  for (let i = 0; i < array.length; i++) {
+    temporary.push(`${array[i]}!!!`);
+  }
+  return temporary;
+}
+
+function iLoveTheBeatles(number) {
+  var temp = [];
+  let i = 0;
+  do {
+    temp.push("I love the Beatles!")
+    i = i + 1;
+  } while (i + number < 15);
+  return temp;
+}
